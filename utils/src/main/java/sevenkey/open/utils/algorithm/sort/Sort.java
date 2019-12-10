20a4160d5803a23c082c33cbbe5be16e0659620b
weijianyu <weijianyu@meituan.com>
2019-12-10 21:07:36 +0800

    快排和寻找第 k 大的数子

--- utils/src/main/java/sevenkey/open/utils/algorithm/sort/Sort.java
+++ utils/src/main/java/sevenkey/open/utils/algorithm/sort/Sort.java
@@ -44,4 +44,99 @@ public class Sort {
             array[l + k] = tempArray[k];
         }
     }
+
+
+    /**
+     * 快排
+     *
+     * @param array 数组
+     * @param l     l
+     * @param r     r
+     */
+    public static void quickSort(int[] array, int l, int r) {
+        if (l >= r) {
+            return;
+        }
+
+        int temp = array[l];
+
+        int i = l;
+        int j = r;
+        while (i < j) {
+
+            while (i < j && array[j] >= temp) {
+                j--;
+            }
+
+            while (i < j && array[i] <= temp) {
+                i++;
+            }
+
+            if (i < j) {
+                int value = array[i];
+                array[i++] = array[j];
+                array[j--] = value;
+            }
+        }
+        array[l] = array[j];
+        array[j] = temp;
+
+        quickSort(array, l, i);
+        quickSort(array, i + 1, r);
+    }
+
+    /**
+     * 寻找第 K 大数字
+     *
+     * @param array 数组
+     * @param l     l
+     * @param r     r
+     * @param k     k
+     * @return 数字
+     */
+    public static int searchK(int[] array, int l, int r, int k) {
+        if (k > r + 1 || k <= 0) {
+            return 0;
+        }
+
+        while (l <= r) {
+            int j = searchK(array, l, r);
+            if (k == (r - j + 1)) {
+                return array[j];
+            } else if (k < (r - j + 1)) {
+                l = j + 1;
+            } else {
+                r = j - 1;
+            }
+        }
+        return 0;
+    }
+
+    private static int searchK(int[] array, int l, int r) {
+        int temp = array[l];
+
+        int i = l;
+        int j = r;
+        while (i < j) {
+
+            while (i < j && array[j] >= temp) {
+                j--;
+            }
+
+            while (i < j && array[i] <= temp) {
+                i++;
+            }
+
+            if (i < j) {
+                int value = array[i];
+                array[i++] = array[j];
+                array[j--] = value;
+            }
+        }
+        array[l] = array[j];
+        array[j] = temp;
+
+        return j;
+    }
 }
+
--- utils/src/test/java/algorithm/sort/SortTest.java
+++ utils/src/test/java/algorithm/sort/SortTest.java
@@ -32,4 +32,42 @@ public class SortTest {
             Assert.assertEquals(cloneArray[i].intValue(), array[i]);
         }
     }
+
+    @Test
+    public void testQuickSort() {
+        int[] array = new int[]{1, 5, 6, 2, 3, 4, 4, 7};
+        List<Integer> cloneArrayList = Lists.newArrayList();
+        for (int i = 0; i < array.length; i++) {
+            cloneArrayList.add(array[i]);
+        }
+
+        Sort.quickSort(array, 0, array.length - 1);
+
+        cloneArrayList.sort(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o1 < o2 ? -1 : o1 == o2 ? 0 : 1;
+            }
+        });
+        Integer[] cloneArray = cloneArrayList.toArray(new Integer[]{});
+
+        for (int i = 0; i < array.length; i++) {
+            Assert.assertEquals(cloneArray[i].intValue(), array[i]);
+        }
+    }
+
+    @Test
+    public void testSearchK() {
+        int[] array = new int[]{1, 5, 6, 2, 3, 4, 4, 7};
+        Assert.assertEquals(0, Sort.searchK(array, 0, array.length - 1, 10));
+        Assert.assertEquals(7, Sort.searchK(array, 0, array.length - 1, 1));
+        Assert.assertEquals(6, Sort.searchK(array, 0, array.length - 1, 2));
+        Assert.assertEquals(5, Sort.searchK(array, 0, array.length - 1, 3));
+        Assert.assertEquals(4, Sort.searchK(array, 0, array.length - 1, 4));
+        Assert.assertEquals(4, Sort.searchK(array, 0, array.length - 1, 5));
+        Assert.assertEquals(3, Sort.searchK(array, 0, array.length - 1, 6));
+        Assert.assertEquals(2, Sort.searchK(array, 0, array.length - 1, 7));
+        Assert.assertEquals(1, Sort.searchK(array, 0, array.length - 1, 8));
+        Assert.assertEquals(0, Sort.searchK(array, 0, array.length - 1, 9));
+    }
 }
